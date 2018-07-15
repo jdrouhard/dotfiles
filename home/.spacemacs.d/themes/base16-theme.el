5d2019c54e36aa15f5299578ddf55d3b41c6b988
John Drouhard <john@jmdtech.org>
2018-07-15 13:25:37 -0500

    remove spacemacs config - i'm not likely going back to emacs at this point

--- home/.spacemacs.d/themes/base16-theme.el
+++
@@ -1,6 +0,0 @@
-(require 'base16-definitions
-         (locate-file "base16-definitions.el" custom-theme-load-path
-                      '("c" "")))
-
-(create-base16-theme base16
-                        base16-description (base16-color-definitions))
