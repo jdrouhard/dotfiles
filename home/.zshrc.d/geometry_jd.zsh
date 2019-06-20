f7d0a758c1da97c9543de064c8e60a545c23370a
John Drouhard <john@jmdtech.org>
2019-06-20 17:11:09 -0500

    zshrc: rewrite zsh theme to use vcs_info (dramatically increases snappiness of prompt)

--- home/.zshrc.d/geometry_jd.zsh
+++ home/.zshrc.d/geometry_jd.zsh
@@ -4,98 +4,113 @@
 # avit: https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/avit.zsh-theme
 # pure: https://github.com/sindresorhus/pure
 
+autoload -Uz vcs_info
+
 PROMPT_SYMBOL='▲'
-EXIT_VALUE_SYMBOL="%{$fg_bold[red]%}△%{$reset_color%}"
+EXIT_VALUE_SYMBOL="%B%F{red}△%f%b"
 RPROMPT_SYMBOL='◇'
+SEPARATOR="%B%F{magenta}::%f%b"
 
-USER_PROMPT_SYMBOL="%{$fg_bold[blue]%}$%{$reset_color%}"
-ROOT_PROMPT_SYMBOL="%{$fg_bold[red]%}#%{$reset_color%}"
+USER_PROMPT_SYMBOL="%B%F{blue}$%f%b"
+ROOT_PROMPT_SYMBOL="%B%F{red}#%f%b"
 
-GIT_DIRTY="%{$fg[red]%}⬡%{$reset_color%}"
-GIT_CLEAN="%{$fg[green]%}⬢%{$reset_color%}"
+USER_HOST="%n@%m"
+ROOT_HOST="%F{red}%m%f"
+
+GIT_DIRTY="%F{red}⬡%f"
+GIT_CLEAN="%F{green}⬢%f"
 GIT_REBASE="\uE0A0"
 GIT_UNPULLED="⇣"
 GIT_UNPUSHED="⇡"
 
-_separator() {
-    echo "%{$fg_bold[magenta]%}::%{$reset_color%}"
+zstyle ':vcs_info:*' enable bzr git hg svn
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' max-exports 3
+zstyle ':vcs_info:*' formats        "%F{242}%b%f" "%u%c"
+zstyle ':vcs_info:*' actionformats  "%F{242}%b%f" "%u%c" "%B%F{yellow}%a %m%f%%b"
+zstyle ':vcs_info:*' patch-format   "[%n/%a]"
+zstyle ':vcs_info:*' nopatch-format "[%n/%a]"
+zstyle ':vcs_info:*' unstagedstr    "$GIT_DIRTY"
+zstyle ':vcs_info:git*' formats        "%c %F{242}%b%f" "%u"
+zstyle ':vcs_info:git*' actionformats  "%c %F{242}%b%f" "%u" "%B%F{yellow}%a %m%f%%b"
+zstyle ':vcs_info:git*' check-for-changes false # handled by git-dirty/git-upstream hooks
+zstyle ':vcs_info:git*+set-message:*' hooks \
+                                        git-hook-begin \
+                                        git-upstream \
+                                        git-dirty
+
+function +vi-git-hook-begin() {
+    if [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) != 'true' ]]; then
+        # hook functions after this will not be called if non-zero is returned
+        return 1
+    fi
 }
 
-_vi_mode_indicator() {
+function +vi-git-upstream() {
+    # This hook targets the 1st message in the format list (branch info)
+    if [[ "$1" != "0" ]]; then
+        return 0
+    fi
+
+    local_commit=$(command git rev-parse @ 2>&1)
+    remote_commit=$(command git rev-parse @{u} 2>&1)
+    common_base=$(command git merge-base @ @{u} 2>&1) # last common commit
+
+    if [[ $local_commit == $remote_commit ]]; then
+        return 0
+    else
+        if [[ $common_base == $remote_commit ]]; then
+            hook_com[staged]="$GIT_UNPUSHED"
+        elif [[ $common_base == $local_commit ]]; then
+            hook_com[staged]="$GIT_UNPULLED"
+        else
+            hook_com[staged]="$GIT_UNPUSHED $GIT_UNPULLED"
+        fi
+    fi
+}
+
+function +vi-git-dirty() {
+    # This hook targets the 2nd message in the format list (%u)
+    if [[ "$1" != "1" ]]; then
+        return 0
+    fi
+
+    hook_com[unstaged]=$GIT_CLEAN
+
+    if test -n "$(command git status --porcelain --ignore-submodules 2>/dev/null)"; then
+        hook_com[unstaged]=$GIT_DIRTY
+    fi
+}
+
+function geometry_prompt() {
     case ${KEYMAP} in
-        (main|viins) TEXT="INSERT"; COLOR="cyan" ;;
-        (vicmd)      TEXT="NORMAL"; COLOR="yellow" ;;
+        (main|viins)   TEXT="INSERT"; COLOR="cyan" ;;
+        (vicmd)        TEXT="NORMAL"; COLOR="yellow" ;;
         (visual|viopp) TEXT="$KEYMAP"; COLOR="magenta" ;;
-        (*)          TEXT="$KEYMAP"; COLOR="" ;;
+        (*)            TEXT="$KEYMAP"; COLOR="" ;;
     esac
-    echo "%{$fg_bold[$COLOR]%}$TEXT%{$reset_color%}"
-}
+    vi_mode_indicator="%B%F{$COLOR}$TEXT%f%b"
 
-_git_dirty() {
-  if test -z "$(git status --porcelain --ignore-submodules 2>/dev/null)"; then
-    echo $GIT_CLEAN
-  else
-    echo $GIT_DIRTY
-  fi
-}
+    local -a messages
 
-_git_rebase_check() {
-  git_dir=$(git rev-parse --git-dir)
-  if test -d "$git_dir/rebase-merge" -o -d "$git_dir/rebase-apply"; then
-    echo "$GIT_REBASE"
-  fi
-}
+    messages+=( "%(?.$PROMPT_SYMBOL.$EXIT_VALUE_SYMBOL)" )        # normal filled triangle or empty red triangle
+    messages+=( "%(!.$ROOT_HOST.$USER_HOST)" )                    # root is a red hostname, user is normal user@hostname
+    messages+=( "$SEPARATOR" )                                    # ::
+    messages+=( "[$vi_mode_indicator]" )                          # [INSERT] or [NORMAL]
+    messages+=( "%F{green}%3~%f" )                                # Current working directory with 3 parent paths
+    messages+=( "%(!.$ROOT_PROMPT_SYMBOL.$USER_PROMPT_SYMBOL) " ) # root is red #, user is blue $
 
-_git_remote_check() {
-  local_commit=$(git rev-parse @ 2>&1)
-  remote_commit=$(git rev-parse @{u} 2>&1)
-  common_base=$(git merge-base @ @{u} 2>&1) # last common commit
+    PROMPT="${(j: :)messages}"
+    PROMPT2=' $RPROMPT_SYMBOL '
 
-  if [[ $local_commit == $remote_commit ]]; then
-    echo ""
-  else
-    if [[ $common_base == $remote_commit ]]; then
-      echo "$GIT_UNPUSHED"
-    elif [[ $common_base == $local_commit ]]; then
-      echo "$GIT_UNPULLED"
-    else
-      echo "$GIT_UNPUSHED $GIT_UNPULLED"
-    fi
-  fi
-}
+    vcs_info
 
-_git_symbol() {
-  echo "$(_git_rebase_check) $(_git_remote_check) "
-}
+    messages=()
+    [[ -n "$vcs_info_msg_2_" ]] && messages+=( "${vcs_info_msg_2_}" $SEPARATOR ) # action info (rebase/merge and [applied/total] patches)
+    [[ -n "$vcs_info_msg_0_" ]] && messages+=( "${vcs_info_msg_0_}" )            # branch info with arrows for ahead/behind upstream
+    [[ -n "$vcs_info_msg_1_" ]] && messages+=( $SEPARATOR "${vcs_info_msg_1_}" ) # clean -> green filled square, dirty -> red empty square
 
-_git_info() {
-  if git rev-parse --git-dir > /dev/null 2>&1; then
-      echo "$(_git_symbol)%F{242}$(git_current_branch)%{$reset_color%} $(_separator) $(_git_dirty)"
-  fi
-}
-
-_user_host() {
-    if [ `whoami` = "root" ]; then
-        echo "%{$fg[red]%}%m%{$reset_color%}"
-    else
-        echo "%n@%m"
-    fi
-}
-
-_prompt_symbol() {
-    if [ `whoami` = "root" ]; then
-        echo "$ROOT_PROMPT_SYMBOL"
-    else
-        echo "$USER_PROMPT_SYMBOL"
-    fi
-}
-
-geometry_prompt() {
-  VIMODE=$(_vi_mode_indicator)
-  PROMPT='%(?.$PROMPT_SYMBOL.$EXIT_VALUE_SYMBOL) $(_user_host) $(_separator) [$VIMODE] %{$fg[green]%}%3~%{$reset_color%} $(_prompt_symbol) '
-
-  PROMPT2=' $RPROMPT_SYMBOL '
-  RPROMPT='%(?..%{$fg[red]%}%? ↵%{$reset_color%} )$(_git_info)'
+    RPROMPT="%(?..%F{red}%? ↵%f ) ${(j: :)messages}"
 }
 
 function zle-line-init zle-keymap-select {
