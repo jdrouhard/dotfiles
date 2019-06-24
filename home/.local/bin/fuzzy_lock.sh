f9608d52afb44b8673981f6fc083a949fda8dda8
John Drouhard <john@jmdtech.org>
2019-06-24 09:27:10 -0500

    i3 updates, simplify fuzzy_lock.sh

--- home/.local/bin/fuzzy_lock.sh
+++ home/.local/bin/fuzzy_lock.sh
@@ -1,26 +1,10 @@
-#!/bin/bash -m
-
-# from http://riccomini.name/posts/linux/2012-09-25-kill-subprocesses-linux-bash/
-kill_child_processes() {
-    local isTopmost=$1
-    local curPid=$2
-    local childPids=`ps -o pid --no-headers --ppid ${curPid}`
-    for childPid in $childPids
-    do
-        kill_child_processes 0 $childPid
-    done
-    if [ $isTopmost -eq 0 ]; then
-        kill -9 $curPid 2>/dev/null
-    fi
-}
-
+#!/bin/bash
 
 if pgrep i3lock > /dev/null; then
     trap - INT TERM EXIT
     exit
 else
     fifo=`find $HOME/.weechat/ -name 'weechat_fifo*'`
-    tmpbg="$(mktemp /tmp/lock-XXXXXXXX.png)"
     away=/tmp/away
 
     # make sure we clean up
@@ -29,9 +13,7 @@ else
             echo "*/away" > $fifo
             rm $away
         fi
-        rm $tmpbg
-        kill_child_processes 1 $$
-        exit
+        pkill -P $$
     }
     trap finish INT TERM EXIT
 
@@ -42,14 +24,8 @@ else
         xset dpms force off
     }
 
-    # create background
-    scrot $tmpbg
-    mogrify -scale 10% -scale 1000% -fill black -colorize 25% $tmpbg
-    #convert $tmpbg -scale 5% -scale 2005% -fill black -colorize 25% $tmpbg
-
-    blank_screen_and_away&
+    blank_screen_and_away &
 
     # lock the screen
-    i3lock -n -i $tmpbg 2>/dev/null
-
+    i3lock -n -k -u --blur 5
 fi
