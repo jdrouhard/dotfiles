88b5efca6d1677983eb6c49a40afb1e042a5de5b
John Drouhard <john@jmdtech.org>
2021-05-05 10:22:47 -0500

    make find_powerline.sh use pip to get the location

--- home/.local/bin/find_powerline.sh
+++ home/.local/bin/find_powerline.sh
@@ -1,13 +1,5 @@
+#!/bin/bash
+
 python_path=$(which python3 2>/dev/null || which python 2>/dev/null)
-
-$python_path <<EOF
-from __future__ import print_function
-
-import site
-import os
-
-for dir in site.getsitepackages():
-    attempt = os.path.join(dir, 'powerline')
-    if os.path.exists(attempt):
-        print(attempt)
-EOF
+packages=$($python_path -m pip show powerline-status 2>/dev/null | grep Location | awk '{print $2}')
+echo $packages'/powerline'
