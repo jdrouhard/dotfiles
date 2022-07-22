429b5533a758cb500298e095dc876c3f635f1900
John Drouhard <john@drouhard.dev>
2022-07-22 08:53:31 -0500

    nvim: update comment config to account for breaking changes

--- home/dot_config/nvim/lua/config/comment.lua
+++ home/dot_config/nvim/lua/config/comment.lua
@@ -13,17 +13,17 @@ local post_hook = function(ctx)
   end
 
   local lcs, rcs = util.parse_cstr({}, ctx)
-  local padding = util.get_padding(use_padding)
+  local padding = util.get_pad(use_padding)
   local partial_block = ctx.cmotion == util.cmotion.char or ctx.cmotion == util.cmotion.v
 
   -- we only care about placeholders when there is an "end" marker for the comment
-  if not rcs then
+  if util.is_empty(rcs) then
     return
   end
 
   local lines = util.get_lines(ctx.range)
 
-  if ctx.cmode == 1 then
+  if ctx.cmode == util.cmode.comment then
     -- comment
     for line_no = 1, #lines do
       local line = lines[line_no]
@@ -33,7 +33,7 @@ local post_hook = function(ctx)
       if line_no == 1 and partial_block then
         scol = ctx.range.scol + lcs:len() + padding:len() + 1
       elseif line_no == 1 or ctx.ctype == 1 then
-        local _, j = line:find(util.escape(lcs) .. padding)
+        local _, j = line:find(vim.pesc(lcs) .. padding)
         scol = j + 1
       end
 
@@ -43,7 +43,7 @@ local post_hook = function(ctx)
           ecol = ecol + lcs:len() + padding:len()
         end
       elseif line_no == #lines or ctx.ctype == 1 then
-        local i, _ = line:find(padding .. util.escape(rcs) .. '$')
+        local i, _ = line:find(padding .. vim.pesc(rcs) .. '$')
         ecol = i - 1
       end
 
@@ -51,16 +51,16 @@ local post_hook = function(ctx)
       local before = line:sub(1, scol-1)
       local after = line:sub(ecol+1)
       line = line:sub(scol, ecol)
-      line = line:gsub(util.escape(lcs), lrepl)
-      line = line:gsub(util.escape(rcs), rrepl)
+      line = line:gsub(vim.pesc(lcs), lrepl)
+      line = line:gsub(vim.pesc(rcs), rrepl)
 
       lines[line_no] = before .. line .. after
     end
-  elseif ctx.cmode == 2 then
+  elseif ctx.cmode == util.cmode.uncomment then
     -- uncomment
     for idx = 1, #lines do
-      lines[idx] = lines[idx]:gsub(util.escape(lrepl), lcs)
-      lines[idx] = lines[idx]:gsub(util.escape(rrepl), rcs)
+      lines[idx] = lines[idx]:gsub(vim.pesc(lrepl), lcs)
+      lines[idx] = lines[idx]:gsub(vim.pesc(rrepl), rcs)
     end
   end
 
