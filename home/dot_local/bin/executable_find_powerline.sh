6555cbc0ed69929a21db31ba1d61a1833bc3f533
John Drouhard <john@drouhard.dev>
2023-12-29 16:25:16 -0600

    tmux: make new tmux sessions open immediately and auto-install powerline-status if missing
    - rewrite the find_powerline helper to be a full python script that
      finds powerline and executes the tmux source-file command to cause
      tmux to initialize the statusbar
    - the script will look for powerline in the user site-packages dir
      first, then try to import powerline and use the module's path, and
      finally (if it still wasn't found), will install powerline-status
      using pip
    - with a valid powerline-status location, the script execs the tmux
      command to source the bindings file directly. this makes the script
      a valid 'run-shell' tmux target by itself
    - run this script in the background, which lets tmux instantly show the
      prompt until powerline is found and initialized, which then "pops in"
      the powerline-status-enabled config
    - change the normal bg/fg colors so there isn't as much "blink" when the
      powerline status pops in

--- home/dot_local/bin/executable_find_powerline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python_path=$(which python3 2>/dev/null || which python 2>/dev/null)
-packages=$($python_path -m pip show powerline-status 2>/dev/null | grep Location | awk '{print $2}')
-echo $packages'/powerline'
---
+++ home/dot_local/bin/executable_tmux_powerline.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import site
+import subprocess
+
+def find_powerline():
+    # this is the fastest way to check for existence of powerline
+    powerline_dir = os.path.join(site.getusersitepackages(), "powerline")
+
+    # if it wasn't found, it might be in a system site-package dir, but we'll
+    # just import it and get its location
+    if not os.path.exists(powerline_dir):
+        powerline_dir = ""
+        try:
+            import powerline
+            powerline_dir = os.path.dirname(powerline.__file__)
+        except:
+            pass
+
+    return powerline_dir
+
+powerline_location = find_powerline()
+
+if not os.path.exists(powerline_location):
+    # powerline likely not installed.
+    powerline_location = ""
+
+    # if still not found, install it with pip
+    import sys
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "powerline-status"])
+        powerline_location = find_powerline()
+    except:
+        pass
+
+if powerline_location != "":
+    subprocess.Popen(['powerline-daemon', '-q'])
+    os.execlp("tmux", "tmux", "source-file", os.path.join(powerline_location, "bindings", "tmux", "powerline.conf"))
