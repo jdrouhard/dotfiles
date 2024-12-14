f50f9e9551be2e65a5265166380fd179509498ca
John Drouhard <john@drouhard.dev>
2024-12-14 11:29:46 -0600

    nvim: update blink config to latest and to act more like nvim-cmp

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -4,6 +4,12 @@ local M = {
   enabled = require('globals').blink_cmp,
   -- dev = true,
 
+  -- use LuaSnip until nvim allows disabling default tab/s-tab keymaps for native snippets
+  dependencies = {
+    'L3MON4D3/LuaSnip',
+    version = 'v2.*',
+  },
+
   version = 'v0.*',
   -- build = 'cargo build --release',
 }
@@ -11,23 +17,41 @@ local M = {
 ---@module 'blink.cmp'
 ---@type blink.cmp.Config
 M.opts = {
+  sources = {
+    cmdline = {}, -- until it's more stable
+  },
+  snippets = {
+    expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+    active = function(filter)
+      if filter and filter.direction then
+        return require('luasnip').jumpable(filter.direction)
+      end
+      return require('luasnip').in_snippet()
+    end,
+    jump = function(direction) require('luasnip').jump(direction) end,
+  },
   keymap = {
     preset = 'default',
     ['<CR>'] = { 'accept', 'fallback' },
+    ['<C-e>'] = { 'hide', 'fallback' },
     ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
     ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
   },
-  windows = {
-    autocomplete = { selection = 'auto_insert', },
-    documentation = { auto_show = true },
+  completion = {
+    list = {
+      selection = 'auto_insert',
+    },
+    documentation = {
+      auto_show = true,
+    },
   },
-  highlight = { use_nvim_cmp_as_default = true, },
-  nerd_font_variant = 'normal',
-
-  -- accept = { auto_brackets = { enabled = true, kind_resolution = { enabled = false } } }
-
-  trigger = { signature_help = { enabled = true, }, },
-  fuzzy = { sorts = { 'score' } }
+  signature = {
+    enabled = true,
+  },
+  appearance = {
+    use_nvim_cmp_as_default = true,
+    nerd_font_variant = 'normal',
+  },
 }
 
 return M
