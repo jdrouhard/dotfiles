08fdbf76500f7b43916897426b29fe0109707b90
John Drouhard <john@drouhard.dev>
2025-01-20 13:11:10 -0600

    more snacks tweaks
    - search UI is a Snacks.input
    - dashboard 'c' is now the chezmoi files picker

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -4,22 +4,29 @@ local M = {
   lazy = false,
 }
 
-local function search()
-  -- vim.ui.input({ prompt = "Grep For", win = { row = -2 } }, function(input)
-  --   Snacks.picker.grep({ live = false, search = input })
-  -- end)
-  local ok, res = pcall(vim.fn.input, "Grep For> ")
-  if ok and res ~= "" then
-    Snacks.picker.grep({ live = false, search = res })
-  end
-end
-
 M.keys = {
   -- bufdelete
-  { "<c-q>",       function() Snacks.bufdelete() end, mode = "", desc = "delete buffer" },
+  { "<c-q>", function() Snacks.bufdelete() end, mode = "", desc = "delete buffer" },
 }
 
 if not require('globals').fzflua then
+  local function search()
+    -- Snacks.picker.grep()
+
+    Snacks.input({
+      prompt = "Grep For",
+      win = {
+        relative = "editor",
+        row = -2,
+        col = false -- false so it merges to a falsey value
+      }
+    }, function(input)
+      if input and input ~= '' then
+        Snacks.picker.grep({ live = false, search = input })
+      end
+    end)
+  end
+
   vim.list_extend(M.keys, {
     -- pickers
     { "<leader>s",   search, { desc = "grep" } },
@@ -70,7 +77,7 @@ M.opts.dashboard = {
       { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
       { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
       { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua require('plugins.chezmoi').pick()" },
       -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
       { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
       { icon = " ", key = "q", desc = "Quit", action = ":qa" },
